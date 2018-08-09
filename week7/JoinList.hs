27c662913a14e2f236a41a32444d6cf46a04147e
rszczers <rafal.szczerski@gmail.com>
2018-08-09 22:16:52 +0200

    Week 7, Ex 1, 2.

---
+++ week7/JoinList.hs
@@ -0,0 +1,32 @@
+import Data.Monoid
+import Sized
+
+-- Ex. 1
+data JoinList m a = Empty
+                  | Single m a
+                  | Append m (JoinList m a) (JoinList m a)
+                  deriving (Eq, Show)
+
+(+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
+(+++) x y = Append (tag x <> tag y) x y
+
+tag :: Monoid m => JoinList m a -> m
+tag Empty = mempty
+tag (Single m a) = m
+tag (Append m _ _) = m
+
+-- Ex. 2
+indexJ :: (Sized b, Monoid b) =>
+          Int -> JoinList b a -> Maybe a
+indexJ _ Empty = Nothing
+indexJ 0 (Single _ a) = Just a
+indexJ 0 (Append _ (Single _ a) _) = Just a
+indexJ 1 (Append _ _ (Single _ b)) = Just b
+indexJ n t@(Append rS l r)
+    | n > jlSize t = Nothing
+    | n <= jlSize l = indexJ n l
+    | otherwise = indexJ (n - jlSize l) r 
+    where jlSize (Append m _ _) = getSize $ size m
+          jlSize Empty = 0
+          jlSize (Single _ _) = 1
+
